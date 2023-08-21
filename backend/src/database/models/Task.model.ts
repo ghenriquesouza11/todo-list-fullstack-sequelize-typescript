d168413c613ff53ff12a96bf03b646de432fc8cd
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-08-21 18:38:12 -0300

    cria o model para receber a tarefa

---
+++ backend/src/database/models/Task.model.ts
@@ -0,0 +1,21 @@
+import { Task } from '../types/Task';
+
+import db from './index';
+
+import { Optional, ModelDefined, DataTypes } from 'sequelize';
+
+export type TaskInpputableFields = Optional<Task, 'id'>;
+
+type TaskModelCreator = ModelDefined<Task, TaskInpputableFields>;
+
+const TaskModel: TaskModelCreator = db.define('Task', {
+    title: DataTypes.STRING,
+    status: DataTypes.STRING,
+    created_at: DataTypes.STRING,
+},
+{
+    timestamps: false,
+    tableName: 'Tasks',
+});
+
+export default TaskModel;
